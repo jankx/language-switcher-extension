29a2ca8858aa01d0e53df01b0c8174bf239ca12f
Puleeno Nguyen <puleeno@gmail.com>
2026-09-25 08:55:40 +0700

    refactor: add backgroundColor attribute to LanguageSwitcher block

--- assets/blocks/language-switcher/build/index.asset.php
+++ assets/blocks/language-switcher/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-server-side-render'), 'version' => '75d178e7e7cd4b74ea11');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-server-side-render'), 'version' => '2519a574e06d72023fbf');
